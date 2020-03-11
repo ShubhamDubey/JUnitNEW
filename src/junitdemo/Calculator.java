38ce905eaca7d62a92cdab7be856cea79a131201
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-03-11 15:43:27 +0530

    MOdified Calculator.java

--- src/junitdemo/Calculator.java
+++ src/junitdemo/Calculator.java
@@ -1,20 +1,34 @@
 package junitdemo;
 
 public class Calculator {
-	public double add(double a,double b){
-		return (a+b);
+
+	public static void main(String[] args) {
+
+		Calculator obj = new Calculator();
+		System.out.println(obj.add(1, 2));
+		System.out.println(obj.subtract(1, 2));
+		System.out.println(obj.multiply(1, 2));
+		System.out.println(obj.divide(5, 5));
+
 	}
-	public double subtract(double a,double b){
-		return (a-b);
+
+	public double add(double a, double b) {
+		return (a + b);
 	}
-	public double multiply(double a,double b){
-		return (a*b);
+
+	public double subtract(double a, double b) {
+		return (a - b);
 	}
-	public double divide(double a,double b){
-		if(b==0){
+
+	public double multiply(double a, double b) {
+		return (a * b);
+	}
+
+	public double divide(double a, double b) {
+		if (b == 0) {
 			throw new ArithmeticException("Error: Attempting to dividew by zero");
 		}
-		return (a/b);
+		return (a / b);
 	}
 
 }
